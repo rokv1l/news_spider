86e2790fc2dd56ba1b0666535cce77ed32cdf6cb
Rokv1l <guzeev.leonid.dc@gmail.com>
2021-05-30 14:46:46 +0300

    tass parser done

--- news_spider/app.py
+++ news_spider/app.py
@@ -1,10 +1,12 @@
 from spider_modules.mskagency import mskagency_parser
+from spider_modules.tass import tass_parser
 from src.sсhedule_mp import IntervalJob, Scheduler
 
 
 def main():
     scheduler = Scheduler()
-    scheduler.add_job(IntervalJob('mskagency', mskagency_parser, delay=120))
+    scheduler.add_job(IntervalJob('mskagency', mskagency_parser, delay=60*30))
+    scheduler.add_job(IntervalJob('tass', tass_parser, delay=60*30))
     scheduler.run_pending()
 
 
