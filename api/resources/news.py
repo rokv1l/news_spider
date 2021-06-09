f66d147748abfa306b4708d3bca488575421f1f1
Rokv1l <guzeev.leonid.dc@gmail.com>
2021-06-09 16:59:36 +0300

    fff

--- api/resources/news.py
+++ api/resources/news.py
@@ -1,5 +1,3 @@
-import json
-
 from flask import request
 from flask_restful import Resource
 from flask_restful import reqparse
@@ -14,9 +12,12 @@ class GetNews(Resource):
         if token != config.token:
             return {'error': 'Authorization failed, wrong token'}, 401
         parser = reqparse.RequestParser()
-        parser.add_argument('source', required=True)
+        parser.add_argument('source', required=False)
         args = parser.parse_args()
-        news = list(news_db_col.find({'source': args['source']}, {'_id': 0}))
+        if args.get('source'):
+            news = list(news_db_col.find({'source': args['source']}, {'_id': 0}))
+        else:
+            news = list(news_db_col.find(dict(), {'_id': 0}))
         if not news:
             return {'error': 'Not results, may be you select wrong source'}, 404
         else:
