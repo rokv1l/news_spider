5977eb43f24bd99d1388f5ec2a2f42cdd21cf13e
Rokv1l <guzeev.leonid.dc@gmail.com>
2021-06-15 22:43:28 +0300

    regnum

--- news_spider/app.py
+++ news_spider/app.py
@@ -14,7 +14,8 @@ def main():
         spider_modules.riamo.riamo_parser, spider_modules.ria.ria_parser, spider_modules.moslenta.moslenta_parser,
         spider_modules.kp.kp_parser, spider_modules.echo.echo_parser, spider_modules.rbc.rbc_parser,
         spider_modules.rt.rt_parser, spider_modules.lenta.lenta_parser, spider_modules.aif.aif_parser,
-        spider_modules.mk.mk_parser, spider_modules.mbk_news.mbk_parser, spider_modules.kommersant.kommersant_parser
+        spider_modules.mk.mk_parser, spider_modules.mbk_news.mbk_parser, spider_modules.kommersant.kommersant_parser,
+        spider_modules.regnum.regnum_parser,
     ]
     scheduler = Scheduler()
     for parser in parsers:
