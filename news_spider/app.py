71e26cff51232eae974beaa4b2f7fe673b17fb2b
Rokv1l <guzeev.leonid.dc@gmail.com>
2021-06-15 15:57:46 +0300

    mk news and cosmetics in app

--- news_spider/app.py
+++ news_spider/app.py
@@ -15,24 +15,11 @@ def main():
         spider_modules.kp.kp_parser, spider_modules.echo.echo_parser, spider_modules.rbc.rbc_parser,
         spider_modules.rt.rt_parser, spider_modules.lenta.lenta_parser
     ]
+    scheduler = Scheduler()
     for parser in parsers:
         process = Process(target=parser)
         process.start()
-    scheduler = Scheduler()
-    scheduler.add_job(IntervalJob('mskagency', spider_modules.mskagency.mskagency_parser, delay=config.run_jobs_delay))
-    scheduler.add_job(IntervalJob('tass', spider_modules.tass.tass_parser, delay=config.run_jobs_delay))
-    scheduler.add_job(IntervalJob('vm', spider_modules.vm.vm_parser, delay=config.run_jobs_delay))
-    scheduler.add_job(IntervalJob('m24', spider_modules.m24.m24_parser, delay=config.run_jobs_delay))
-    scheduler.add_job(IntervalJob('icmos', spider_modules.icmos.icmos_parser, delay=config.run_jobs_delay))
-    scheduler.add_job(IntervalJob('mockva', spider_modules.mockva.mockva_parser, delay=config.run_jobs_delay))
-    scheduler.add_job(IntervalJob('riamo', spider_modules.riamo.riamo_parser, delay=config.run_jobs_delay))
-    scheduler.add_job(IntervalJob('ria', spider_modules.ria.ria_parser, delay=config.run_jobs_delay))
-    scheduler.add_job(IntervalJob('moslenta', spider_modules.moslenta.moslenta_parser, delay=config.run_jobs_delay))
-    scheduler.add_job(IntervalJob('kp', spider_modules.kp.kp_parser, delay=config.run_jobs_delay))
-    scheduler.add_job(IntervalJob('echo', spider_modules.echo.echo_parser, delay=config.run_jobs_delay))
-    scheduler.add_job(IntervalJob('rbc', spider_modules.rbc.rbc_parser, delay=config.run_jobs_delay))
-    scheduler.add_job(IntervalJob('rt', spider_modules.rt.rt_parser, delay=config.run_jobs_delay))
-    scheduler.add_job(IntervalJob('lenta', spider_modules.lenta.lenta_parser, delay=config.run_jobs_delay))
+        scheduler.add_job(IntervalJob(parser.__name__, parser, delay=config.run_jobs_delay))
     scheduler.run_pending()
 
 
