27d522770c7af2e80e756374e3e0be89da3693f0
Rokv1l <guzeev.leonid.dc@gmail.com>
2021-06-16 21:54:40 +0300

    rg

--- news_spider/app.py
+++ news_spider/app.py
@@ -2,7 +2,7 @@ from multiprocessing import Process, freeze_support
 
 import config
 from spider_modules import aif, bfm, echo, icmos, kommersant, kp, lenta, m24, mbk_news, mk, mockva, moslenta, mskagency
-from spider_modules import novayagazeta, pravda, rbc, regnum, ria, riamo, rt, tass, vm, interfax
+from spider_modules import novayagazeta, pravda, rbc, regnum, ria, riamo, rt, tass, vm, interfax, rg
 from src.sсhedule_mp import IntervalJob, Scheduler
 
 freeze_support()
@@ -14,7 +14,7 @@ def main():
         mockva.mockva_parser, riamo.riamo_parser, ria.ria_parser, moslenta.moslenta_parser, kp.kp_parser,
         echo.echo_parser, rbc.rbc_parser, rt.rt_parser, lenta.lenta_parser, aif.aif_parser, mk.mk_parser,
         mbk_news.mbk_parser, kommersant.kommersant_parser, regnum.regnum_parser, novayagazeta.novayagazeta_parser,
-        pravda.pravda_parser, bfm.bfm_parser, interfax.interfax_parser,
+        pravda.pravda_parser, bfm.bfm_parser, interfax.interfax_parser, rg.rg_parser,
     ]
     scheduler = Scheduler()
     for parser in parsers:
