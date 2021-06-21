a40c7bddbec200988602cd673f663df4fd714045
Rokv1l <guzeev.leonid.dc@gmail.com>
2021-06-21 15:35:59 +0300

    f

--- bot/app.py
+++ bot/app.py
@@ -7,12 +7,13 @@ import config
 from init_users import init_users
 from bot_modules import common, stats, errors
 
+updater = Updater(token=config.telegram_token, use_context=True)
+dispatcher = updater.dispatcher
+
 
 if __name__ == '__main__':
     init_users()
 
-    updater = Updater(token=config.telegram_token, use_context=True)
-    dispatcher = updater.dispatcher
     dispatcher.add_handler(common.authorize_handler)
     dispatcher.add_handler(stats.stats_handler)
 
