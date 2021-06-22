c5207e196c86537174f69a315b4e220dd7285abb
Rokv1l <guzeev.leonid.dc@gmail.com>
2021-06-22 17:27:34 +0300

    f

--- bot/bot_modules/errors.py
+++ bot/bot_modules/errors.py
@@ -7,6 +7,7 @@ from src.database import errors_db_col
 
 def errors_mailing():
     while True:
+        time.sleep(mailing_sleep)
         if not messaging_list:
             continue
         cursor = errors_db_col.find({'checked': False})
@@ -14,10 +15,11 @@ def errors_mailing():
         for i in cursor:
             errors_db_col.update_one(i, {'$set': {'checked': True}})
             errors.add(i['error'])
+        if not errors:
+            continue
         text = '\n-------------------------------\n'.join(errors)
         for _id in messaging_list:
             dispatcher.bot.send_message(
                 chat_id=_id,
                 text=text
             )
-        time.sleep(mailing_sleep)
