65a12c842f51cc90af1c7eb1770e9eb40e78b432
leo <guzeev.leonid.dc@gmail.com>
2021-10-02 10:11:09 +0300

    f

--- news_spider/parser_modules/parsers.py
+++ news_spider/parser_modules/parsers.py
@@ -10,7 +10,6 @@ from src.database import news_db_col
 from config import newspaper_config
 
 
-@logger.catch()
 def page_parser(url):
     try:
         article = newspaper.Article(url, language='ru', config=newspaper_config)
@@ -23,23 +22,11 @@ def page_parser(url):
         return None
 
 
-@logger.catch()
 def portal_parser(url):
     news_paper = newspaper.build(url, language='ru', memoize_articles=False)
 
-    # Возможно этот блок кода не нужен
-    pattern_1 = r'https?:\/\/.+\.\w+($|\/)'
-    pattern_2 = r'\w+\.\w+($|\/)'
-    domen = re.search(pattern_1, url)
-    if not domen:
-        logger.warning(f'Something went wrong, domen is None. Url is {url}')
-        return
-    domen = re.search(pattern_2, domen.group()).group()[:-1]
-    # --------------------------------
     tmp_count = 0
     for article in news_paper.articles:
-        if domen not in article.url:
-            continue
         if news_db_col.find_one({'url': article.url}):
             continue
         data = page_parser(article.url)
