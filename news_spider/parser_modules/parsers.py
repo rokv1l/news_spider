edb94cea094b12053d71bdeba2ddedf43be60c03
Leo <guzeev.leonid.dc@gmail.com>
2022-02-10 16:27:31 +0300

    f

--- news_spider/parser_modules/parsers.py
+++ news_spider/parser_modules/parsers.py
@@ -31,25 +31,23 @@ def portal_parser(url):
         count = 0
         with MongoClient(mongo_ip, mongo_port) as client:
             for article in news_paper.articles:
-                while True:
-                    try:
-                        if client.news_parser.news.find_one({'url': article.url}):
-                            continue
-                        data = page_parser(article.url)
-                        if data == 404 or not data or not data[1] or not data[0]:
-                            continue
-                        if not data[2] or data[2].replace(tzinfo=None) > datetime.now().replace(tzinfo=None):
-                            data[2] = datetime.now().isoformat()
-                        client.news_parser.news.insert_one({
-                            'source': url,
-                            'url': article.url,
-                            'title': data[0],
-                            'content': data[1],
-                            'datetime': data[2]
-                        })
-                        break
-                    except (AutoReconnect, ServerSelectionTimeoutError):
-                        pass
+                try:
+                    if client.news_parser.news.find_one({'url': article.url}):
+                        continue
+                    data = page_parser(article.url)
+                    if data == 404 or not data or not data[1] or not data[0]:
+                        continue
+                    if not data[2] or data[2].replace(tzinfo=None) > datetime.now().replace(tzinfo=None):
+                        data[2] = datetime.now().isoformat()
+                    client.news_parser.news.insert_one({
+                        'source': url,
+                        'url': article.url,
+                        'title': data[0],
+                        'content': data[1],
+                        'datetime': data[2]
+                    })
+                except (AutoReconnect, ServerSelectionTimeoutError):
+                    logger.error('db error')
                 count += 1
                 sleep(config.request_delay)
     except Exception:
